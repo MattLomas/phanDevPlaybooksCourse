df397f29f2b5d5f2f1ab469f6daf835749c52112
DevLab-Admin Matt Lomas <matthew.lomas@adarma.com>
2019-10-31 12:34:14 +0000

    v1.0-L4-T1-S6 Lab 4 Task 1 Step 6 (includes prior step completion).

--- Case Promotion Lab.py
+++ Case Promotion Lab.py
@@ -8,6 +8,121 @@ from datetime import datetime, timedelta
 
 def on_start(container):
     phantom.debug('on_start() called')
+    
+    # call 'apiPromoteToCase' block
+    apiPromoteToCase(container=container)
+
+    return
+
+def apiPromoteToCase(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('apiPromoteToCase() called')
+
+    phantom.promote(container=container, template="Data Breach")
+    filterFixedsourceDNS(container=container)
+
+    return
+
+def filterFixedsourceDNS(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filterFixedsourceDNS() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.sourceDnsDomain", "!=", ""],
+        ],
+        name="filterFixedsourceDNS:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        filterFixedFilePath(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+def filterFixedFilePath(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filterFixedFilePath() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.filePath", "!=", ""],
+        ],
+        name="filterFixedFilePath:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        filterFixedAddress(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+def filterFixedAddress(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filterFixedAddress() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.destinationAddress", "!=", ""],
+        ],
+        name="filterFixedAddress:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        format_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+def format_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('format_1() called')
+    
+    template = """A file has been detected that has been
+determined to be potentially malicious. A case
+has been opened.
+Case link: {0}
+Event name: {1}
+Description: {2}
+Source URL: {3}
+Target Server IP: {4}
+Suspicious File Path: {5}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "container:url",
+        "container:name",
+        "container:description",
+        "filtered-data:filterFixedsourceDNS:condition_1:artifact:*.cef.sourceDnsDomain",
+        "filtered-data:filterFixedAddress:condition_1:artifact:*.cef.destinationAddress",
+        "filtered-data:filterFixedFilePath:condition_1:artifact:*.cef.filePath",
+    ]
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_1")
+
+    actSendEmail(container=container)
+
+    return
+
+def actSendEmail(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('actSendEmail() called')
+
+    # collect data for 'actSendEmail' call
+    formatted_data_1 = phantom.get_format_data(name='format_1')
+
+    parameters = []
+    
+    # build parameters list for 'actSendEmail' call
+    parameters.append({
+        'from': "edu-labserver@splunk.com",
+        'to': "matthew.lomas@adarma.com",
+        'cc': "",
+        'bcc': "",
+        'subject': "Phantom Course: New Case Created",
+        'body': formatted_data_1,
+        'attachments': "",
+        'headers': "",
+    })
+
+    phantom.act("send email", parameters=parameters, assets=['smtp'], name="actSendEmail")
 
     return
 
